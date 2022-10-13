c5d39f3433ee5497d764bd114794828668b25283
Monique Costa <costanicce@gmail.com>
2022-10-13 13:24:10 -0300

    first commit

---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,46 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Text, TextProps } from './Text';
+
+export default {
+    title: 'Components/Text',
+    component: Text,
+    args: {
+        children: 'Lorem ipsum.',
+    }
+} as Meta<TextProps>;
+
+export const Default: StoryObj<TextProps> = {
+};
+
+export const Small: StoryObj<TextProps> = {
+    args: {
+        size: 'sm',
+    }
+};
+
+export const Large: StoryObj<TextProps> = {
+    args: {
+        size: 'lg',
+    }
+};
+
+export const CustomComponent: StoryObj<TextProps> = {
+    args: {
+        asChild: true,
+        children: (
+            <p>Same but inside a p tag</p>
+        )
+    },
+    argTypes: {
+        children: {
+            table: {
+                disable: true,
+            }
+        },
+        asChild: {
+            table: {
+                disable: true,
+            }
+        }
+    }
+};
